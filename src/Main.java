a103a90bb804380b3899e7ac0e5231be660f69d1
ljkey <lj.key.scut@gmail.com>
2016-04-02 11:01:51 +0800

    初始化项目

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by ljkey on 2016/3/29.
+ */
+public class Main {
+    public static void main(String[] args){
+        String key = "0000001010010110010010001100010000111000001100000011100001100100";
+        byte[] key_byte = key.getBytes();
+        String plaintext1 = "0000000000000000000000000000000000000000000000000000000000000000";
+        String plaintext2 = "1000000000000000000000000000000000000000000000000000000000000000";
+    }
+}
